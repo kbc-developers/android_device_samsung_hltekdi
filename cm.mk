d8c12582be6fb6c7a1a3d23a2bce4ca2d696c91b
blackzigong <blackzigong@gmail.com>
2014-01-10 17:47:38 +0900

    Add missing optimizations for hltekdi
    Change-Id: I05eead04df404ea37c7a4cd46dda395885651bab

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltekdi TARGET_DEVICE=hltekdi BUILD_FINGERPRINT="KDDI/SCL22/SCL22:4.3/JSS15J/SCL22KDUBML1:user/release-keys" PRIVATE_BUILD_DESC="hltekdi-user 4.3 JSS15J SCL22KDUBML1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCL22 TARGET_DEVICE=hltekdi BUILD_FINGERPRINT="KDDI/SCL22/SCL22:4.3/JSS15J/SCL22KDUBML1:user/release-keys" PRIVATE_BUILD_DESC="hltekdi-user 4.3 JSS15J SCL22KDUBML1 release-keys"
 
 
 PRODUCT_NAME := cm_hltekdi
