72561e0c456f768d696fbe7af3dc576c41eadefd
black <blackzigong@gmail.com>
2014-01-14 03:55:50 -0800

    Fix assertion 2
    Change-Id: I31e7c0f1e1115a02d93964c719be5fcbb5ab81fb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@
 -include vendor/samsung/hltekdi/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := SCL22
+TARGET_OTA_ASSERT_DEVICE := SCL22,hltekdi
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hltekdi_defconfig
