50c96e03c5b9d63cc742e8ef8e4530aac22123e2
sakuraba <sakura.bar.com@gmail.com>
2014-09-10 19:44:26 +0900

    Merge branch 'cm-11.0' of git:CyanogenMod/android_device_samsung_hltevzw

--- init/init_hlte.c
+++ init/init_hlte.c
@@ -56,7 +56,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     if (strstr(bootloader, "SCL22")) {
         /* hltekdi */
-        cdma_properties("1");
         property_set("ro.build.fingerprint", "KDDI/SCL22/SCL22:4.4.2/KOT49H/SCL22KDU2FNF7:user/release-keys");
         property_set("ro.build.description", "hltekdi-user 4.4.2 KOT49H SCL22KDU2FNF7 release-keys");
         property_set("ro.product.model", "SCL22");
